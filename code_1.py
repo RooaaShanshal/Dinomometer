bbb2fb0e068f32676968b2b4f78a473acadced46
RooaaShanshal <shanshar@mcmaster.ca>
2024-03-30 17:58:47 -0400

    Final draft

--- code_1.py
+++ code_1.py
@@ -1,22 +1,11 @@
-import RPi.GPIO as GPIO
+import gpiozero import LED
 from lcd_api import LcdApi
 from i2c_lcd import I2cLcd
 from hx711 import HX711
 import sys
 import time
 
-LED_1 = 21 # green
-LED_2 = 25 # green
-LED_3 = 20 # yellow
-LED_4 = 19 # yellow
-LED_5 = 26 # red
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_1, GPIO.out)
-GPIO.setup(LED_2, GPIO.out)
-GPIO.setup(LED_3, GPIO.out)
-GPIO.setup(LED_4, GPIO.out)
-GPIO.setup(LED_4, GPIO.out)
+LED = [LED(21), LED(25), LED(20), LED(19), LED(26)]
 
 I2C_ADDR = 0x27
 I2C_NUM_ROWS = 4
@@ -24,110 +13,98 @@ I2C_NUM_COLS = 16
 
 lcd = I2cLcd(1, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
-threshold = 0.5 #CHANGE
-
-# Code below from: https://github.com/j-dohnalek/hx711py/blob/master/calibration.py
-
-# START OF REFERENCED CODE
 hx = HX711(6, 5, 128, 'A')
 
+Offset = 0
+Ratio = 1
 
 def cleanAndExit():
+    lcd.clear()
+    lcd.putstr("Cleaning up...")
     print("Cleaning up...")
-    GPIO.cleanup()
+    for led in LED:
+        led.off()
+    lcd.clear()
+    lcd.putstr("Shutting down, Bye!")
+    time.sleep(5)
     lcd.clear()
     print("Bye!")
-    sys.exit()
 
-def setup():
-    """
-    code run once
-    """
-    print("Initializing.\n Please ensure that the scale is empty.")
-    scale_ready = False
-    while not scale_ready:
-        if (GPIO.input(hx.DOUT) == 0):
-            scale_ready = False
-        if (GPIO.input(hx.DOUT) == 1):
-            print("Initialization complete!")
-            scale_ready = True
 
 def calibrate():
-    readyCheck = input("Remove any items from scale. Press any key when ready.")
-    offset = hx.read_average()
-    print("Value at zero (offset): {}".format(offset))
-    hx.set_offset(offset)
-    print("Please place an item of known weight on the scale.")
-
-    readyCheck = input("Press any key to continue when ready.")
-    measured_weight = (hx.read_average()-hx.get_offset())
-    item_weight = input("Please enter the item's weight in grams.\n>")
-    scale = int(measured_weight)/int(item_weight)
-    hx.set_scale(scale)
-    print("Scale adjusted for grams: {}".format(scale))
-
-def get_value(time):
-    grip = []
-    lcd.putstr("Hold down!")
-    while(time>0):
-        grip.append(hx.get_grams())
-        time -= 1
-    lcd.move_to(3, 1)
-    lcd.putstr("Let go!")
     lcd.clear()
-    return max(grip)
+    lcd.putstr("Remove items from strain gauge, press any key to continue")
+    readyCheck = input("Remove items from Strain gauge, press any key to continue")
+    Offset = int(hx.get_raw_data(5))/5
+    lcd.clear()
+    lcd.putstr("place an object w/ known weight, press any key when ready")
+    readyCheck = input("Place an object with a known weight on strain gauge, press any key when ready")
+    measured = (hx.get_raw_data(5))/5
+    print("Done")
+    lcd.clear()
+    lcd.putstr("Enter known weight into terminal")
+    weight = int(input("Enter known weight of object here"))
+    Ratio = (int(measured)-Offset)/weight
+    lcd.clear()
+    lcd.putstr("Calibration complete")
+
+def get_kg(value):
+    return value/Ratio
+
+def get_average(time):
+    return get_kg(sum(hx.get_raw_data(time))/time)
+
 
 def dino():
+    lcd.clear()
+    lcd.putstr("Enter number of tests in terminal")
     numTests = int(input("Input number of tests desired"))
     value = 0
-    LED_counter = 1
-    totalTests = numTests
-    while(numTests>0):
-        measure = get_value(3)
-        while(measure<=threshold):
-            #no response from dynomometer
-            measure = get_value(3)
-        value+=measure
-        LED = "LED_"+LED_counter # String? maybe it shouldn't be
-        GPIO.output(LED, GPIO.HIGH)
-        numTests -= 1
-    message_1 = "Number of tests conducted:"+totalTests 
-    message_2 = "Average force: "+ value/totalTests
+    LEDcounter = 0
+    testCounter = numTests
+
+    while(testCounter>0):
+        lcd.clear()
+        lcd.putstr("Hold strain gauge, press any key to continue")
+        readyCheck = input("Hold strain gauge, press any key to continue")
+        measured = get_average(3)
+        lcd.clear()
+        lcd.putstr("Measurement taken!")
+        value+=measured
+        time.sleep(5)
+        lcd.clear()
+        lcd.putstr("Force: "+str(measured))
+
+        led_now = LED[LEDcounter]
+        led_now.on()
+
+        LEDcounter += 1
+        testCounter -= 1
+
+        time.sleep(5)
+
+    message_1 = "Number of tests conducted:"+str(numTests)+"\n" 
+    message_2 = "Average force: "+ str(value/numTests)+"\n"
+    print(message_1)
+    print(message_2)
     lcd.clear()
     lcd.putstr(message_1)
-    lcd.move_to(3, 1)
     lcd.putstr(message_2)
-    time.sleep(10)
-    # GPIO.output(LED_1, GPIO.LOW)
-    # GPIO.output(LED_2, GPIO.LOW)
-    # GPIO.output(LED_3, GPIO.LOW)
-    # GPIO.output(LED_4, GPIO.LOW)
-    # GPIO.output(LED_5, GPIO.LOW)
-    # GPIO.cleanup()
 
 
 
-def loop():
-    """
-    code run continuously
-    """
+def loop(): #code run continuously
     try:
         prompt_handled = False
         while not prompt_handled:
-            #val = hx.get_grams()
-            #hx.power_down()
-            #time.sleep(.001)
-            #hx.power_up()
-            #print("Item weighs {} grams.\n".format(val))
             choice = input("Please choose:\n"
-                           "[1] Recalibrate.\n"
+                           "[1] Calibrate.\n"
                            "[2] Start the Dinomometer\n"
                            "[0] Clean and exit.\n>")
             if choice == "1":
                 calibrate()
             elif choice == "2":
                 dino()
-                #print("\nOffset: {}\nScale: {}".format(hx.get_offset(), hx.get_scale()))# CHANGE 
             elif choice == "0":
                 prompt_handled = True
                 cleanAndExit()
@@ -136,9 +113,8 @@ def loop():
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
 
-#END OF REFERENCED CODE
         
 
-
+loop()
 
 
